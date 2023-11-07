430567d17cb0a3a415eeceb28c9f12a4d97e8129
anuragpampati <48316130+anuragpampati@users.noreply.github.com>
2023-11-07 10:07:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+var rand1 = Math.floor(Math.random()*6) + 1;
+var rand2 = Math.floor(Math.random()*6) + 1;
 
+var i1= document.getElementsByTagName("img")[0];
+var i2= document.getElementsByTagName("img")[1];
 
-var k = document.getElementById("bu");
-k.style.color = "red";
+i1.setAttribute("src",("./images/"+ "dice" + rand1 +".png"));
+i2.setAttribute("src",("./images/"+ "dice" + rand2 +".png"));
+
+var h = document.getElementsByTagName("h1");
+
+if (rand1>rand2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!!";
+}
+if(rand1<rand2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!!";
+}
+if(rand1 == rand2){
+    document.querySelector("h1").innerHTML = "Draw!!!";
+}
